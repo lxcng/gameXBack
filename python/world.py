520b5966e9c8c790090d87cc8709a5fc8d8c98dd
alex <zaqepa@gmail.com>
2015-10-05 23:22:11 +0300

    +world

---
+++ python/world.py
@@ -0,0 +1,20 @@
+
+
+class World:
+    """World class"""
+
+
+
+
+    def __init__(self, arg):
+        self
+
+
+        self.arg = arg
+
+
+
+class Body:
+    """Body class"""
+    def __init__(self, arg):
+        self.arg = arg
