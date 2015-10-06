76192f01ff22d2ee88a8145120719f6fb764860e
alex <zaqepa@gmail.com>
2015-10-06 22:01:36 +0300

    +

--- python/world.py
+++ python/world.py
@@ -6,25 +6,45 @@ import matplotlib.pyplot as plt
 class World:
     """World class"""
 
-    def __init__(self, arg):
-        self.children = {}
-        self.arg = arg
+    ticks_per_frame = 50
+
+    def __init__(self):
+        self.dynamic = {}
+        self.static = {}
 
     def add_child(self, child):
-        self.children.update({child.id, child})
+        self.dynamic.update({child.id, child})
+        child.set_parent(this)
 
     def del_child(self, id):
-        self.children.pop(id)
+        self.dynamic.pop(id)
+
+    def step(self):
+        for c in self.dynamic:
+            c.step()
+
+    def check_collisions(self):
+        return 0
 
 
 class Body:
     """Body class"""
 
-    def __init__(self, id, shape, position):
+    def __init__(self, id, shape, position): #, static):
         self.id = id
         self.shape = shape
         self.position = position
         self.angle = 0.0
+        self.velocity = (0, 0)
+        self.parent = 0
+        # self.static = static
+
+
+    def step(self):
+        self.position += self.velocity
+
+    def set_parent(self, parent):
+        self.parent = parent
 
 
 class Polygon:
@@ -33,18 +53,21 @@ class Polygon:
     def __init__(self, anchor, vertices):
         self.anchor = anchor
         self.vertices = vertices
+        self.angle = 0.0
 
     def rotate(self, angle):
         self.vertices -= self.anchor
-        s = math.sin(angle)
-        c = math.cos(angle)
+        delta = angle - self.angle
+        self.angle = angle
+        s = math.sin(delta)
+        c = math.cos(delta)
         for i in np.arange(self.vertices.shape[0]):
             x = self.vertices[i, 0]
             y = self.vertices[i, 1]
             self.vertices[i] = [x * c - y * s,
                                 x * s + y * c]
         self.vertices += self.anchor
-        return  0
+        # return  0
 
     @staticmethod
     def circle(anchor, radius):
